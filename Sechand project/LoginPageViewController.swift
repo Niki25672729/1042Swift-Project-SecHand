ee54387135d174ca7e26785ddd3493e2ba895843
phuang <n25672729@gamil.com>
2016-05-06 22:12:36 +0800

    new file:   Sechand project.xcodeproj/project.pbxproj 	new file:   Sechand project.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   Sechand project.xcodeproj/project.xcworkspace/xcuserdata/Niki.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Sechand project.xcodeproj/project.xcworkspace/xcuserdata/levi.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Sechand project.xcodeproj/xcuserdata/Niki.xcuserdatad/xcschemes/Sechand project.xcscheme 	new file:   Sechand project.xcodeproj/xcuserdata/Niki.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   Sechand project.xcodeproj/xcuserdata/levi.xcuserdatad/xcschemes/Sechand project.xcscheme 	new file:   Sechand project.xcodeproj/xcuserdata/levi.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   Sechand project/AppDelegate.swift 	new file:   Sechand project/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   Sechand project/Assets.xcassets/Contents.json 	new file:   Sechand project/Assets.xcassets/logo.imageset/Contents.json 	new file:   Sechand project/Assets.xcassets/logo.imageset/SecHand-logo 1x.png 	new file:   Sechand project/Assets.xcassets/logo.imageset/SecHand-logo 2x.png 	new file:   Sechand project/Assets.xcassets/logo.imageset/SecHand-logo 3x.png 	new file:   Sechand project/Base.lproj/LaunchScreen.storyboard 	new file:   Sechand project/Base.lproj/Main.storyboard 	new file:   Sechand project/Info.plist 	new file:   Sechand project/LoginPageViewController.swift

---
+++ Sechand project/LoginPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginPageViewController.swift
+//  Sechand project
+//
+//  Created by Ｎiki25672729 on 5/6/16.
+//  Copyright © 2016 levi. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController {
+    @IBAction func CancleButton(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true) {}
+    }
+}
