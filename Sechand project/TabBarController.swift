c984fe0970a2aa2bf18ee3cd0494b29fd909864d
phuang <n25672729@gamil.com>
2016-05-08 22:30:37 +0800

    The basic storyboard  Changes to be committed: 	modified:   Sechand project.xcodeproj/project.xcworkspace/xcuserdata/Niki.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Sechand project/Base.lproj/Main.storyboard 	new file:   Sechand project/PageViewController.swift 	new file:   Sechand project/TabBarController.swift

---
+++ Sechand project/TabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarController.swift
+//  Sechand project
+//
+//  Created by Ｎiki25672729 on 5/8/16.
+//  Copyright © 2016 levi. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    @IBAction func TriangleButtonClicked(sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
